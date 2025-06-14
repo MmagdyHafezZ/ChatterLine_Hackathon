6f6a808cb8ade85b42f2890ed0de99ace5ea1134
Wilbur <85022787+Pupper0n1@users.noreply.github.com>
2025-06-14 13:30:50 -0400

    m

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,17 @@
 // src/app.ts
 import express, { Request, Response } from 'express';
 
+import prisma from './prisma';
+
 const app = express();
 const port = process.env.PORT || 3000;
 
+var store = {}
 
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', async (req: Request, res: Response) => {
+    console.log(await prisma.user.count())
+
     res.send('Hello, Express with TypeScript!');
 });
 
