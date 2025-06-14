aeab2a4fc5cb93b8a9d86f41d3546579f868f598
Wilbur <85022787+Pupper0n1@users.noreply.github.com>
2025-06-14 11:49:43 -0400

    Added backend

---
+++ backend/src/app.ts
@@ -0,0 +1,13 @@
+// src/app.ts
+import express, { Request, Response } from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello, Express with TypeScript!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
